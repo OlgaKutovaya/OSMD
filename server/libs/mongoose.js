10a4a0879c5a8545781ef95f3d379f98146c0022
Igor <380931547944@ex.ua>
2017-03-06 23:48:36 +0200

    site
    osmd db site

--- server/libs/mongoose.js
+++
@@ -1,9 +0,0 @@
-const
-  mongoose = require('mongoose'),
-  config = require('../config/config');
-
-// mongoose.set('debug', true);
-mongoose.Promise = require('bluebird');
-mongoose.connect(config.mongoose.local.host);
-
-module.exports = mongoose;
--- server/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const apiRouter = require('express').Router();
-
-apiRouter.use('/users', require('./users.router'));
-apiRouter.use('/products', require('./product.router.js'));
-apiRouter.use('/server-status', require('./server-status.router'));
-
-module.exports = apiRouter;
