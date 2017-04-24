310ac031e635e93f2f7bd10d9bd1335cce09f982
Vitalii Doroshenko <vetalpro.exe@gmail.com>
2017-04-24 15:42:09 +0300

    migrate to angular 4

--- client/osmd-web/src/app/app.module.ts
+++ client/osmd-web/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import {
@@ -24,6 +25,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     HttpModule,
     BrowserModule,
+    BrowserAnimationsModule,
     ServiceModule,
     ConfirmDialogModule,
     GrowlModule,
